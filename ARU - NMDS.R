12db4f28d906079581976b1aa9e33ad8148d0260
ZaDackVis <60154953+ZaDackVis@users.noreply.github.com>
2020-08-10 18:43:27 -0700

    Add files via upload

--- ARU - NMDS.R
+++ ARU - NMDS.R
@@ -1,6 +1,3 @@
-###############
-####ARU NMDS-ings###
-###############
 
 ##GOAL - conduct a nmds for each zone. 
 ##Is there a difference in community strcture between zones
@@ -8,6 +5,7 @@
 library(vegan)
 library(ggplot2)
 library(tidyverse)
+library(ggordiplots)
 
 setwd("~/R/Yukon_Birds")
 
@@ -28,7 +26,6 @@ Species <- WF_ARU_PC[!names(WF_ARU_PC) %in% c("location","site","zone","stat_num
 Species$GCRF <- NULL #a single obs of GCRF may be removed if I want to...
 
 #####Getting matrix in order######
-ggord.PCA
 #I need to aggregate all point-count locations by zone, and sum raw abundance...
 
 ALL_Sites_by_Zone <- WF_ARU_PC %>% 
@@ -44,42 +41,24 @@ ALL_Sites_by_Zone <- separate(ALL_Sites_by_Zone, Site_Zone,
                               c("site", "zone"), 
                              sep = "_", FALSE, FALSE, "warn")
 
-######Running the  the NMDS#####
-ALL_Zones_ARU_MDS <- metaMDS(ALL_Sites_by_Zone[names(Species)], "bray",k = 2, autotransform = FALSE)
+######Running the  the NMDS for ELEVATION#####
 
 
-#Plot it basic
-plot(ALL_Zones_ARU_MDS,type="n")
-ordipointlabel(ALL_Zones_ARU_MDS,
-               display="site",
-               cex=.5)
 
 
-#encircle the surface at the 95% confidence ellipses  
-ordi.ellipse <- ordiellipse(ALL_Zones_ARU_MDS,
-            groups = ALL_Sites_by_Zone$zone, 
-            col=1:3,
-            lwd=3)
+ARU_MDS <- metaMDS(ALL_Sites_by_Zone[names(Species)], "bray",k = 2, autotransform = FALSE)
+stressplot(ARU_MDS)
 
-
-#Put the spiders on it!
-ordispider(ALL_Zones_ARU_MDS,
-           groups = ALL_Sites_by_Zone$zone, 
-           col=1:3,
-           lwd=3)
-
-#What about a sweet topographical map!?
-
-
-ARU_elev <- YK_Env[c("Site_Zone","elev")] #create a vector of site elevations.
+#adding env data to the matrix.
+ARU_elev <- YK_Env[c("Site_Zone","elev", "Tmax_sm")] #create a vector of site elevations.
 
 
 ARU_elev <- ARU_elev %>% group_by(ARU_elev$Site_Zone) %>% 
-             summarise_at(c('elev'), mean, na.rm = FALSE)   #take the mean.
+             summarise_at(c('elev','Tmax_sm'), mean, na.rm = FALSE)   #take the mean.
 
 names(ARU_elev)[names(ARU_elev)=="ARU_elev$Site_Zone"] <- "Site_Zone" 
 
-ARU_MDS_scores <- as.data.frame(scores(ALL_Zones_ARU_MDS))  #make a dataframe of the NMDS coordinates
+ARU_MDS_scores <- as.data.frame(scores(ARU_MDS))  #make a dataframe of the NMDS coordinates
 
 
 ALL_Sites_by_Zone <- column_to_rownames(ALL_Sites_by_Zone,"Site_Zone")  #populate the new dataframe with descriptive varriables.
@@ -94,11 +73,50 @@ ARU_MDS_scores <- separate(ARU_MDS_scores, Site_Zone,
 ARU_MDS_scores <- left_join(ARU_MDS_scores, ARU_elev, by = "Site_Zone")
 
 
+
+#############Base graphics###########
+plot(ARU_MDS,type="n")
+ordipointlabel(ARU_MDS,
+               display="sites",
+               cex=.5)
+
+ordi.ellipse <- ordiellipse(ARU_MDS,
+                            groups = ALL_Sites_by_Zone$zone, 
+                            col=1:3,
+                            lwd=3)
+
+
+#Put the spiders on it!
+ordispider(ARU_MDS,
+           groups = ALL_Sites_by_Zone$zone, 
+           col=1:3,
+           lwd=3)
 ordisurf(ARU_MDS_scores[c("NMDS1","NMDS2")],ARU_MDS_scores$elev, add = TRUE)  #project a surface to the plot based on mean elevation. 
 
+Elev_Fit <- envfit(ARU_MDS_scores[c("NMDS1","NMDS2")], ARU_MDS_scores$Tmax_sm, permutations = 999, strata=NULL, choices=c(1,2),display = "species")
 
-########Doing it all and plotting it with GGPLOT##########
+plot(Elev_Fit, add = TRUE)
 
+####GGordination with ggordiplot###
+
+#plot points with elev vector
+GGElev_Fit <- gg_envfit(ord = ARU_MDS_scores[c("NMDS1","NMDS2")],env = ARU_MDS_scores$elev,perm = 1000,groups = ARU_MDS_scores$Zone)
+ 
+#plot ppints with max summer temp as vector.
+gg_envfit(ord = ARU_MDS_scores[c("NMDS1","NMDS2")], env = ARU_MDS_scores$Tmax_sm,perm = 1000,groups = ARU_MDS_scores$Zone)
+
+
+#Ordiplot of NMDS with ellipses and spiders.
+gg_ordiplot(ord = ARU_MDS_scores[c("NMDS1","NMDS2")],groups = ARU_MDS_scores$Zone, ellipse = TRUE,spiders = TRUE)
+
+
+
+#gg ordisurf (cannot manipulate colors.)
+
+gg_ordisurf(ord = ARU_MDS_scores[c("NMDS1","NMDS2")],env = ARU_MDS_scores$elev)
+
+
+#########Hacked Ordi_surf using GGplot for color changing. #####
 #I need a geom for the topo map.
 
 ordi <- ordisurf(ARU_MDS_scores[c("NMDS1","NMDS2")],ARU_MDS_scores$elev, add = TRUE) #butcher the function so I can see all the parts of it...
@@ -111,38 +129,12 @@ ordi.xyz$z <- as.vector(ordi.grid$z) #extracts Z coordinates...
 
 ordi.xyz <- data.frame(na.omit(ordi.xyz)) #creates a dataframe with no NAs.
 
+rm(ordi.grid)
+rm(ordi)
 
-
-################
-graph <- ggplot()+
+GGElev_Surf <- ggplot()+geom_point(ARU_MDS_scores, mapping = aes(x=NMDS1, y=NMDS2))+
   stat_contour(data = ordi.xyz, aes(x = x, y = y, z = z, color =(..level..)), binwidth = 25)+ #adds the contour lines.
-  scale_colour_gradient(high = "red", low = "green")
+  scale_colour_gradient(high = "darkorange", low = "darkgreen")
 
 
-graph + geom_point(ARU_MDS_scores, mapping = aes(x=NMDS1, y=NMDS2))
-
-
-
-#colors the contour lines
-#  geom_point(ARU_MDS_scores, mapping = aes(x=NMDS1, y=NMDS2)) #plot survey points
- 
-
-
-#plots the ordisurf
-
-  
-  
-#####################
-
-#ggordi <- ggplot()+ geom_point(BorealMDS_Points, mapping = aes(x=NMDS1, y=NMDS2, color="darkgreen")) +
-#  geom_point(AlpineMDS_Points,mapping = aes(x=NMDS1, y=NMDS2, color="red"))+
-#  geom_point(SubalpineMDS_Points,mapping = aes(x=NMDS1, y=NMDS2, color="blue"))+
-#  stat_contour(data = ordi.xyz, aes(x = x, y = y, z = z, color = rev(15)))+
-#  scale_colour_gradient(high = "darkgreen", low = "darkolivegreen1")
-
-
-
-#ggordi
-
-
-#scale_color_manual(values=c("red", "blue", "darkgreen"))
+GGElev_Surf
